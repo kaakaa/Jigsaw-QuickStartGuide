6ec750de6f12f22c231062e7c91c432a51589fd3
kaakaa <stooner.hoe@gmail.com>
2015-09-13 23:11:09 +0900

    sample: Packaging

---
+++ README.md
@@ -0,0 +1,5 @@
+# JDK9 Project Jigsaw: Module System Quick-Start Guide Sample
+
+[Project Jigsaw: Quick Start Guide](http://openjdk.java.net/projects/jigsaw/quick-start "Project Jigsaw: Quick Start Guide")
+
+é åoÅB
--- build.bat
+++ build.bat
@@ -1,4 +1,11 @@
+@echo off
+
 mkdir mods mods\com.greetings mods\org.astro
 
 javac -d mods\org.astro src\org.astro\module-info.java src\org.astro\org\astro\World.java
 javac -modulepath mods -d mods\com.greetings src\com.greetings\module-info.java src\com.greetings\com\greetings\Main.java
+
+mkdir mlib
+
+jar --create --archive=mlib\org.astro@1.0.jar --module-version=1.0 -C mods\org.astro .
+jar --create --archive=mlib\com.greetings.jar --main-class=com.greetings.Main -C mods\com.greetings .
